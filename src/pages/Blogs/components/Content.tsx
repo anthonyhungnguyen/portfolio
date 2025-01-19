a27a9ba1af0c0290ade0182eb512f1205a0114d0
hungnp <hungnp1@ghn.vn>
2025-01-18 19:06:31 -0500

    fix: blog content responsive

--- src/pages/Blogs/components/Content.tsx
+++ src/pages/Blogs/components/Content.tsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-function BlogDetails({ blogData }: { blogData: BlogType }) {
+function Content({ blogData }: { blogData: BlogType }) {
     const [markdown, setMarkdown] = useState('')
     useEffect(() => {
         axios.get(blogData.content).then((response) => {
@@ -16,31 +16,33 @@ function BlogDetails({ blogData }: { blogData: BlogType }) {
     }, [blogData.content])
 
     return (
-        <Markdown
-            className='prose prose-invert flex flex-col'
-            remarkPlugins={[[remarkGfm], [remarkToc]]}
-            components={{
-                code(props) {
-                    const { children, className, ...rest } = props
-                    const match = /language-(\w+)/.exec(className || '')
-                    return match ? (
-                        <SyntaxHighlighter
-                            PreTag='div'
-                            children={String(children).replace(/\n$/, '')}
-                            language={match[1]}
-                            style={dark}
-                        />
-                    ) : (
-                        <code {...rest} className={className}>
-                            {children}
-                        </code>
-                    )
-                },
-            }}
-        >
-            {markdown}
-        </Markdown>
+        <div className='max-w-full overflow-x-auto px-8'>
+            <Markdown
+                className='prose prose-invert flex flex-col max-w-full'
+                remarkPlugins={[[remarkGfm], [remarkToc]]}
+                components={{
+                    code(props) {
+                        const { children, className, ...rest } = props
+                        const match = /language-(\w+)/.exec(className || '')
+                        return match ? (
+                            <SyntaxHighlighter
+                                PreTag='div'
+                                children={String(children).replace(/\n$/, '')}
+                                language={match[1]}
+                                style={dark}
+                            />
+                        ) : (
+                            <code {...rest} className={className}>
+                                {children}
+                            </code>
+                        )
+                    },
+                }}
+            >
+                {markdown}
+            </Markdown>
+        </div>
     )
 }
 
-export default BlogDetails
+export default Content
